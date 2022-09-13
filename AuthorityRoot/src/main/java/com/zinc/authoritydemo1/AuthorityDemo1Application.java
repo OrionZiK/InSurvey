2a49fd9c37ad7686a68cbb852c201f074771e1b3
OrionZiK <103878886+OrionZiK@users.noreply.github.com>
2022-09-13 15:34:34 +0800

    Add files via upload

---
+++ AuthorityRoot/src/main/java/com/zinc/authoritydemo1/AuthorityDemo1Application.java
@@ -0,0 +1,13 @@
+package com.zinc.authoritydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityDemo1Application.class, args);
+    }
+
+}
---
+++ AuthorityRoot/src/test/java/com/zinc/authoritydemo1/AuthorityDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinc.authoritydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorityDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
