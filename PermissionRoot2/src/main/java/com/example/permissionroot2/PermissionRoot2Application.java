6473469556d0682f04733a22d228e9a34d1ceb35
OrionZiK <103878886+OrionZiK@users.noreply.github.com>
2022-10-20 16:25:04 +0800

    Add files via upload

---
+++ PermissionRoot2/src/main/java/com/example/permissionroot2/PermissionRoot2Application.java
@@ -0,0 +1,13 @@
+package com.example.permissionroot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionRoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionRoot2Application.class, args);
+    }
+
+}
---
+++ PermissionRoot2/src/test/java/com/example/permissionroot2/PermissionRoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.permissionroot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionRoot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
