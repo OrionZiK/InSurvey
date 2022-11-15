e37daf1876fd31ad2fecd43ca5d3dc4f394f4013
OrionZiK <103878886+OrionZiK@users.noreply.github.com>
2022-11-15 11:25:10 +0800

    Add files via upload

---
+++ SpringBootTest-kuang/springBootWeb03/src/main/java/com/zinc/springbootweb03/SpringBootWeb03Application.java
@@ -0,0 +1,18 @@
+package com.zinc.springbootweb03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeb03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWeb03Application.class, args);
+        System.out.println("sadasdad");
+        System.out.println("sadasdad");
+        System.out.println("sadasdad");
+        System.out.println("sadasdad");
+        System.out.println("sadasdad");
+    }
+
+}
---
+++ SpringBootTest-kuang/springBootWeb03/src/test/java/com/zinc/springbootweb03/SpringBootWeb03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinc.springbootweb03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWeb03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
