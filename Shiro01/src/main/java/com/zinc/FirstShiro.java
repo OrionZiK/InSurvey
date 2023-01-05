063bb2ed8d60e05948491d559c3e2dea0f0a1567
OrionZiK <103878886+OrionZiK@users.noreply.github.com>
2023-01-05 10:16:11 +0800

    Add files via upload

--- Shiro01/src/main/java/com/zinc/FirstShiro.java
+++ Shiro01/src/main/java/com/zinc/FirstShiro.java
@@ -1,18 +1,15 @@
 package com.zinc;
 
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.IncorrectCredentialsException;
-import org.apache.shiro.authc.LockedAccountException;
-import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.logging.Logger;
 
 /**
  * @author OrionZinc
@@ -26,7 +23,7 @@ public class FirstShiro {
 
         Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
         SecurityManager securityManager = factory.getInstance();
-        SecurityUtils.setSecurityManager(SecurityManager);
+        SecurityUtils.setSecurityManager(securityManager);
 
         // get the currently executing user:
         Subject currentUser = SecurityUtils.getSubject();
